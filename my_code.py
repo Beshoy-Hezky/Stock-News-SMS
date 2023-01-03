e43a14f6ab0f032365f5be82d5393013f11247d6
Beshoy-Hezky <beshoyhezky@gmail.com>
2023-01-03 18:29:03 -0500

    feat: get yesterday's and before yesterday's code and calculate percentage difference

--- my_code.py
+++ my_code.py
@@ -11,3 +11,14 @@ url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&sym
 req = requests.get(url)
 information = req.json()["Time Series (Daily)"]
 info_list = [value for (key,value) in information.items()]
+
+#yesterdays closing price 
+yest_close_price = info_list[0]["4. close"]
+
+#before yesterdays closing price
+before_yesterday = info_list[1]["4. close"]
+
+# %difference  = |new-old|/old
+perc_difference = abs(float(before_yesterday)-float(yest_close_price))/ float(before_yesterday)*100
+rounded_perc_difference = round(perc_difference,2)
+print(rounded_perc_difference)
