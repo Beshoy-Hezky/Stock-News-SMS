b85a13b58a5ee875aacd8bdd41be65dc7a34c7f6
Beshoy-Hezky <beshoyhezky@gmail.com>
2023-01-03 18:24:15 -0500

    feat: add API calls

--- my_code.py
+++ my_code.py
@@ -2,3 +2,12 @@ import requests
 
 #Insert whatever company stock you want here
 stock_name = "TSLA"
+
+#get you own API key by logging into alphavantage.co
+my_API_key = "8FJ0NDXIHSNTIQNL"
+
+#endpoint provided in the documentation 
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + stock_name + "&apikey=" + my_API_key
+req = requests.get(url)
+information = req.json()["Time Series (Daily)"]
+info_list = [value for (key,value) in information.items()]
